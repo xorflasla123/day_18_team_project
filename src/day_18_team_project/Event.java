e00e670ad82dec5c6a881bf99b74eda0c0c0b2d4
sharon <sharon@kim.com>
2021-03-04 18:54:45 +0900

    기본 틀 완성

---
+++ src/day_18_team_project/Event.java
@@ -0,0 +1,23 @@
+package day_18_team_project;
+
+public class Event implements Manage {
+
+	@Override
+	public void register() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void search() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void back() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/day_18_team_project/MainClass.java
@@ -0,0 +1,93 @@
+package day_18_team_project;
+
+import java.util.Scanner;
+/*
+1. 회원관리	- 동민님(Member 클래스)
+-등록 [회원번호 (이름 전화번호)] Hashmap<Integer, Member(class)>
+-검색
+-돌아가기
+
+2. 물품관리	- 도현님(Product 클래스)
+-등록 [물품번호 (이름 수량)] Hashmap<Integer, Product(class)>
+-검색
+-돌아가기
+
+3. 이벤트관리	- 택림님
+-등록 [날자(월/일) 이름] Hashmap<String, String>
+-검색
+-돌아가기
+
+4. 종료
+*/
+interface Manage {
+	public void register();	// 등록
+	public void search();	// 검색
+	public void back();		// 돌아가기
+}
+
+public class MainClass {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num, su;
+		while(true) {
+			System.out.println("1. 회원관리 2. 물품관리 3. 이벤트관리 4. 종료");
+			System.out.print(">>> ");
+			num = input.nextInt();
+			switch(num) {
+				case 1:		// 회원관리
+					System.out.println("회원 관리입니다.");
+					System.out.println("1. 회원등록 2. 회원검색 3. 돌아가기");
+					System.out.print(">>> ");
+					su = input.nextInt();
+					switch(su) {
+						case 1:		// 등록
+							
+							break;
+						case 2:		// 검색
+							
+							break;
+						case 3:		// 돌아가기
+							
+							break;
+					}
+					break;
+				case 2:		// 물품관리
+					System.out.println("물품 관리입니다.");
+					System.out.println("1. 물품등록 2. 물품검색 3. 돌아가기");
+					System.out.print(">>> ");
+					su = input.nextInt();
+					switch(su) {
+					case 1:		// 등록
+						
+						break;
+					case 2:		// 검색
+						
+						break;
+					case 3:		// 돌아가기
+						
+						break;
+					}
+					break;
+				case 3:		// 이벤트관리
+					System.out.println("이벤트 관리입니다.");
+					System.out.println("1. 이벤트등록 2. 이벤트검색 3. 돌아가기");
+					System.out.print(">>> ");
+					su = input.nextInt();
+					switch(su) {
+					case 1:		// 등록
+						
+						break;
+					case 2:		// 검색
+						
+						break;
+					case 3:		// 돌아가기
+						
+						break;
+					}
+					break;
+				case 4:
+					System.exit(0);
+			}
+		}
+	}
+}
