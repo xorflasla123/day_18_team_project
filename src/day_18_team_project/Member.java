88fb6666223ed24443cfce4ef3a1603dd2bd32cd
BDM <ehdalseorka2@gmail.com>
2021-03-04 19:51:28 +0900

    회원관리 구현 완료

--- src/day_18_team_project/Member.java
+++ src/day_18_team_project/Member.java
@@ -1,17 +1,47 @@
 package day_18_team_project;
 
-public class Member implements Manage {
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class Member implements Manage {
+	
+	Scanner input = new Scanner(System.in);
+	
+	HashMap<String, Mem> map = new HashMap<String,Mem>();
+	
+	String name = null, addr = null, sex = null, birth = null;
 	@Override
 	public void register() {
-		// TODO Auto-generated method stub
 		
+		
+		System.out.print("등록 이름 : ");
+		name = input.next();
+		System.out.print("등록 주소 : ");
+		addr = input.next();
+		System.out.print("등록 성별 : ");
+		sex = input.next();
+		System.out.print("등록 생년월일 : ");
+		birth = input.next();
+		Mem m = new Mem();
+		m.setName(name);
+		m.setAddr(addr);
+		m.setSex(sex);
+		m.setBirth(birth);
+		map.put(name, m);		
 	}
 
 	@Override
 	public void search() {
-		// TODO Auto-generated method stub
+		System.out.println("검색 이름 : ");
+		name = input.next();
+		Mem search = map.get(name);
 		
+		if(search != null) {
+			System.out.println("이름 : "+search.getName());
+			System.out.println("주소 : "+search.getAddr());
+			System.out.println("성별 : "+search.getSex());
+			System.out.println("생년월일 : "+search.getBirth());
+		}
 	}
 
 	@Override
