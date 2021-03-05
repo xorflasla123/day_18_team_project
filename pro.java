18e1bebbd547e026ae36ceaef261bc63dde75917
als6207 <lisa6688@naver.com>
2021-03-05 18:23:16 +0900

    관리기능구현

---
+++ pro.java
@@ -0,0 +1,37 @@
+
+
+	package day_18_team_project;
+	/*2. 물품관리
+	-등록 [물품번호 (이름 수량)] Hashmap<Integer, Product(class)>
+	-검색
+	-돌아가기
+	*/
+
+	public class pro {
+		private String name;
+		private Integer itemCount;
+		private Integer itemNo;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public Integer getItemCount() {
+			return itemCount;
+		}
+		public void setItemCount(Integer itemCount) {
+			this.itemCount = itemCount;
+		}
+		public Integer getItemNo() {
+			return itemNo;
+		}
+		public void setItemNo(Integer itemNo) {
+			this.itemNo = itemNo;
+		}
+	
+
+	}
+
+
+
